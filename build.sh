587fdd01f2ace967a2abf6d2fff1b3392853c5e5
Konstantin Rudenskii <atrd.konstantin@gmail.com>
2023-02-24 10:36:45 +0300

    Add comment

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-export CC=/usr/bin/clang 
-export CXX=/usr/bin/clang++ 
-export LDFLAGS=-L/opt/homebrew/lib 
-export CPPFLAGS=-I/opt/homebrew/include 
-export CFLAGS=-I/opt/homebrew/include 
-make
+# On apple intel macs these paths may differ
 
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+export LDFLAGS=-L/opt/homebrew/lib
+export CPPFLAGS=-I/opt/homebrew/include
+export CFLAGS=-I/opt/homebrew/include
+make
